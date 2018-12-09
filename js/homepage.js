101da81900f8bbb17415706902cd3dc44b3f1271
Galen Bowen <galenbowen@gmail.com>
2018-12-08 22:55:37 -0800

    rearranging home page

--- js/homepage.js
+++ js/homepage.js
@@ -15,13 +15,15 @@ next.onclick = function() {
 };
 
 //if the window is scrolled past mid-way, pause the slide-show
-window.addEventListener("scroll", scrollPause);
+/*
+indow.addEventListener("scroll", scrollPause);
 
 function scrollPause() {
 	if (window.pageYOffset > 400) {
 		pauseSlideShow();
 	}
 }
+*/
 
 //if previous button is clicked, pause the slideshow and go back to previous slide
 previous.onclick = function() {
@@ -95,49 +97,7 @@ function goToSlide(n) {
 	slides[resetSlide].className = 'slide';
 }
 
-//---------------------------------- Product Slider ----------------------------//
 
-var running = true;
-var thumbSelect = document.querySelectorAll('#slider-thumbnails > div');
-var thumbGroup = document.getElementById('slider-thumbnails');
-
-thumbGroup.onclick = function() {
-	if (running = true) {
-		pauseAdvance();
-	}
-}
-
-function pauseAdvance() {
-	running = false;
-	clearInterval(picInterval);
-}
-// function resumeAdvance() {
-// 	running = true;
-// 	picInterval = setInterval(nextPic, 4000);
-// }
-
-var bigPic = document.querySelectorAll('.indiv-img');
-var currentPic = 0;
-var infoBox = document.querySelectorAll('#slider-product-info > div');
-var currentBox = 0;	
-var currentThumb = 0;
-var picInterval = setInterval(nextPic, 4000);
-
-function nextPic() {
-	goToProduct(currentPic+1);
-}
-
-function goToProduct(n) {
-	bigPic[currentPic].className = 'indiv-img bottom';
-	currentPic = (n+bigPic.length)%bigPic.length;
-	bigPic[currentPic].className = 'indiv-img middle';
-	infoBox[currentBox].className = 'info-right';
-	currentBox = (n+infoBox.length)%infoBox.length;
-	infoBox[currentBox].className = 'info-center';
-	thumbSelect[currentThumb].className = '';
-	currentThumb = (n+thumbSelect.length)%thumbSelect.length;
-	thumbSelect[currentThumb].className = 'highlighted';	
-}
 
 //__________________________ Blog Slider __________________________________//
 var blogSlides = document.querySelectorAll('.blogslide');
---
+++ svsinc/product-slider.php
@@ -0,0 +1,74 @@
+		<div id="product-slider" class="col30 center-hor flex flex-wrap lghalf smfull sm3">
+			<div id="slider-images" class="flex flex-row">
+				<div id="slider-thumbnails" class="flex flex-column">
+					<div onclick="goToProduct(0)">
+						<img src="assets/images/products/88826-80x80.png"/>
+					</div>
+					<div onclick="goToProduct(1)">
+						<img src="assets/images/products/99937-80x80.png"/>					
+					</div>
+					<div onclick="goToProduct(2)">
+						<img src="assets/images/products/g2637-80x80.png"/>					
+					</div>
+					<div onclick="goToProduct(3)">
+						<img src="assets/images/products/bc826-80x80.png"/>					
+					</div>
+					<div onclick="goToProduct(4)">
+						<img src="assets/images/products/bc937-80x80.png"/>					
+					</div>				
+				</div>
+				<div id="slider-product-image">
+					<div id="image-group">
+						<a href="#" class="indiv-img middle">
+							<img src="assets/images/products/88826-400x400.png"/>
+						</a>
+						<a href="#" class="indiv-img">
+							<img src="assets/images/products/99937-400x400.png"/>
+						</a>
+						<a href="#" class="indiv-img">
+							<img src="assets/images/products/g2637-400x400.png"/>
+						</a>
+						<a href="#" class="indiv-img">
+							<img src="assets/images/products/bc826-400x400.png"/>
+						</a>
+						<a href="#" class="indiv-img">
+							<img src="assets/images/products/bc937-400x400.png"/>
+						</a>					
+					</div>
+				</div>
+			</div>
+			<div id="slider-product-info">
+				<div class="info-center">
+					<a href="#">
+					<h3>Fine-Grain Finishing Salt, 8oz Pouch</h3>
+					<h3>$11.99</h3>
+					</a>
+				</div>
+				<div>
+					<a href="#">
+					<h3>Extra-Coarse Grinder Salt, 8oz Pouch</h3>
+					<h3>$11.99</h3>
+					</a>
+				</div>
+				<div>
+					<a href="#">
+					<h3>Finishing Salt and Grinder Salt Gift Pack</h3>
+					<h3>$22.99</h3>
+					</a>
+				</div>
+				<div>
+					<a href="#">
+					<h3>Fine-Grain Finishing Salt, 4lb Bulk Bag</h3>
+					<h3>$45.99</h3>
+					</a>
+				</div>
+				<div>
+					<a href="#">
+					<h3>Extra-Coarse Grinder Salt, 4lb Bulk Bag</h3>
+					<h3>$45.99</h3>
+					</a>
+				</div>
+			</div>
+		</div>
+
+	<script src="../js/productslider.js"></script>
