b920f65fb35ba4ae7361f764c52fe90e00944208
Galen Bowen <galenbowen@gmail.com>
2019-01-01 18:04:45 -0600

    added socket

--- svsinc/svsfooter.php
+++ svsinc/svsfooter.php
@@ -2,7 +2,17 @@
 	<div class="layout-row">
 	</div>
 </div>
-<div class="layout-container bg-dark-wine">
+<div class="layout-container bg-dark-wine around center-vert socket wrap">
+	<div class="center-vert around">
+		<img src="/assets/images/condor-logo-white-60.png" />
+		<span>&copy;2016 - Sacred Valley Salt Co.</span>
+	</div>
+	<div class="center-vert">
+		<span><a href="#">Privacy Policy</a></span>
+		<span>&nbsp;|&nbsp;</span>
+		<span><a href="#">Terms of Use</a></span>
+	</div>
+		<a href="login.php">Log In</a>
 </div>
 
 <script src="../js/footerscript.js"></script>
